7806802f8f55d45ccd9638e72a18d2a3ae341f5f
istiakdev <istiakahmed.dev346@gmail.com>
2025-12-10 10:23:19 -0500

    update

--- js/script.js
+++ js/script.js
@@ -4,3 +4,26 @@
                 time: 1000
             });
         });
+
+        
+//==== Back-to-top button
+  $(window).on('scroll', function(event) {
+    if($(this).scrollTop() > 600){
+        $('.back-to-top').fadeIn(200)
+    } else{
+        $('.back-to-top').fadeOut(200)
+    }
+});
+//==== Animate the scroll to top
+$('.back-to-top').on('click', function(event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: 0,
+    }, 1000);
+});
+
+// Animation 
+    AOS.init({
+        
+    });
